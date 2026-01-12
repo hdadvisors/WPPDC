e3f468aefb395826c5a2a4ed415d2273caf789e1
mtedougherty <125600190+mtedougherty@users.noreply.github.com>
2026-01-12 14:25:39 -0500

    franklin zoning lookup

--- raw data/zoning look up table creation.R
+++ raw data/zoning look up table creation.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 
+## Zoning ordinances have hyphenated districts, but the parcel data is not. Therefore, save the lookup table in a way that can be used with parcel data.
+
 # Danville zoning lookup table
 danville_zoning <- data.frame(
   locality = "Danville",
@@ -42,3 +44,43 @@ danville_zoning <- data.frame(
 # Save it
 write.csv(danville_zoning, "analyzed data/danville_zoning_lookup.csv", row.names = FALSE)
 
+# Franklin County zoning lookup table (codes without hyphens)
+franklin_zoning <- data.frame(
+  locality = "Franklin County",
+  zone_code = c("A1", "RE", "R1", "R2", "RC1", "C1", "RMF", "RPD",
+                "B1", "B2", "GB",
+                "M1", "M2",
+                "PCD", "REP",
+                "SML", "SM", "POS", "NZ"),
+  zone_name = c("Agricultural District",
+                "Residential Estates District",
+                "Residential Suburban Subdivision District R-1",
+                "Residential Suburban Subdivision District R-2",
+                "Residential Combined Subdivision District",
+                "Residential Combined Subdivision District",  # C1 same as RC-1
+                "Residential Multifamily District",
+                "Residential Planned Development District",
+                "Business District, Limited",
+                "Business District, General",
+                "Business District, General",  # GB same as B-2
+                "Industrial District, Light Industry",
+                "Industrial District, Heavy Industry",
+                "Planned Commercial Development",
+                "Regional Enterprise Park District",
+                "Smith Mountain Lake Surface District",
+                "Smith Mountain",
+                "Parks/Open Space",
+                "Not Zoned"),
+  zone_category = c("Agricultural", 
+                    "Residential", "Residential", "Residential", "Residential", 
+                    "Residential", "Residential", "Residential",
+                    "Commercial", "Commercial", "Commercial",
+                    "Industrial", "Industrial",
+                    "Commercial", "Industrial",
+                    "Overlay/Special", "Overlay/Special", "Parks/Open Space", 
+                    "Unzoned"),
+  stringsAsFactors = FALSE
+)
+
+# Save it
+write.csv(franklin_zoning, "analyzed_data/franklin_zoning_lookup.csv", row.names = FALSE)
